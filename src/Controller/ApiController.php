b1d01824f672264a8640000462bf2e3b59d13da0
Layeche Torki <layeche.torki@gmail.com>
2022-03-31 15:09:06 +0200

    new controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,8 +3,9 @@
 namespace App\Api;
 
 use DateTime;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 
@@ -18,21 +19,44 @@ class ApiController
 
     /**
      * Function post
-     * 
+     *
      * @Route("/post", name="post_rdv", methods={"POST"})
-     * 
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function post()
+    public function post(Request $request)
     {
-        return new JsonResponse([]);
+
+        $data = json_decode($request->getContent());
+        if (empty($data)) {
+            return new JsonResponse(['error' => 'empty data']);
+        }
+        $date = $date = new DateTime();
+        $date->setTimestamp($data['date']);
+        $askedDate =  $date->format('U = Y-m-d H:i:s');
+
+        $trackfood = $this->getDoctrine()->getRepository(Foodtrucks::class)->findBy(['registration' => $data['registration']]);
+        dd($trackfood);
+    }
+    /**
+     * @Route("/get")
+     *
+     * @return void
+     */
+    public function get()
+    {
     }
 
 
-
-    private function isFriday(DateTime $now)
+    /**
+     * @param DateTime $date
+     * @return bool
+     */
+    private function isFriday(DateTime $date)
     {
-        $date = $now->format('Y-m-d');
-        $day = date('w', strtotime($date));
+        $current = $date->format('Y-m-d');
+        $day = date('w', strtotime($current));
 
         if ($day == 5) {
             return true;
@@ -41,6 +65,10 @@ class ApiController
         return false;
     }
 
+    /**
+     * @param DateTime $date
+     * @return bool
+     */
     private function isDateInCurrentWeek(DateTime $date)
     {
         $firstDay = date("Y-m-d", strtotime('sunday last week'));
