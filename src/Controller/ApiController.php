0e5e8efc7b4afa4f2a920a77c8898b2eba48c80e
Layeche Torki <layeche.torki@gmail.com>
2022-03-31 14:19:41 +0200

    add methode post

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,9 +2,14 @@
 
 namespace App\Api;
 
+use DateTime;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
 
 /**
- * class 
+ * Class 
  * 
  * @Route("/api")
  */
@@ -14,9 +19,38 @@ class ApiController
     /**
      * Function post
      * 
-     * @Route("/post", name="post_rdv")
+     * @Route("/post", name="post_rdv", methods={"POST"})
+     * 
      */
     public function post()
     {
+        return new JsonResponse([]);
+    }
+
+
+
+    private function isFriday(DateTime $now)
+    {
+        $date = $now->format('Y-m-d');
+        $day = date('w', strtotime($date));
+
+        if ($day == 5) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function isDateInCurrentWeek(DateTime $date)
+    {
+        $firstDay = date("Y-m-d", strtotime('sunday last week'));
+        $lastDay = date("Y-m-d", strtotime('sunday this week'));
+        $date->format("Y-m-d");
+
+        if ($date > $firstDay && $date < $lastDay) {
+            return true;
+        }
+
+        return false;
     }
 }
