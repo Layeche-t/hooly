17d8f5292fea3485d0e088bd694beb05e1f4387c
Layeche Torki <layeche.torki@gmail.com>
2022-03-31 11:20:21 +0200

    controller api

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Api;
+
+
+/**
+ * class 
+ * 
+ * @Route("/api")
+ */
+class ApiController
+{
+
+    /**
+     * Function post
+     * 
+     * @Route("/post", name="first_post")
+     */
+    public function post()
+    {
+    }
+}
